f5ae670aeac631e33276a22b83b1213ae8fd616c
Morgan Davies <morgandavies2020@gmail.com>
2023-03-09 15:23:00 +0000

    Command line format with mvn:exec java functional

---
+++ src/main/java/com/bonk/Bonk.java
@@ -0,0 +1,65 @@
+package com.bonk;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import javax.sound.sampled.*;
+import javax.sound.sampled.LineEvent.Type;
+
+/**
+ * Hello world!
+ *
+ */
+public class Bonk
+{
+    public static void main( String[] args )  {
+        class AudioListener implements LineListener {
+            private boolean done = false;
+            @Override public synchronized void update(LineEvent event) {
+                Type eventType = event.getType();
+                if (eventType == Type.STOP || eventType == Type.CLOSE) {
+                    done = true;
+                    notifyAll();
+                }
+            }
+            public synchronized void waitUntilDone() throws InterruptedException {
+                while (!done) { wait(); }
+            }
+        }
+
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    System.out.println("Opening BONK...");
+                    File bonkFile = new File("src/main/resources/bonk.mp3");
+                    System.out.println("Getting BONK stream...");
+                    InputStream bonkStream = new FileInputStream(bonkFile);
+                    InputStream bufferedBonkStream = new BufferedInputStream(bonkStream);
+                    System.out.println("Assigning BONK stream...");
+                    AudioListener listener = new AudioListener();
+                    AudioInputStream inputStream = AudioSystem.getAudioInputStream(bufferedBonkStream);
+                    try {
+                        Clip clip = AudioSystem.getClip();
+                        clip.addLineListener(listener);
+                        clip.open(inputStream);
+                        try {
+                            clip.start();
+                            listener.waitUntilDone();
+                            System.out.println("BONK!");
+                        } finally {
+                            clip.close();
+                            System.out.println("Closing Clip...");
+                        }
+                    } finally {
+                        inputStream.close();
+                        System.out.println("Closing Stream...");
+                    }
+                } catch (Exception e) {
+                    System.err.println(e.getMessage());
+                }
+                System.out.println("Done!");
+            }
+        }).start();
+    }
+}
